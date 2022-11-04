ceefee6d2665b6731a9c49d9a8b297af54758acf
amantarento <amankumar.sharma@tarento.com>
2022-11-04 15:23:55 +0530

    add the postaddress details

--- project/ws/app/src/lib/routes/home/services/utility.service.ts
+++ project/ws/app/src/lib/routes/home/services/utility.service.ts
@@ -11,13 +11,16 @@ export class UtilityService {
       _.filter(data.content, { isDeleted: false }).forEach((user: any) => {
         // tslint:disable-next-line
         const professionalDetails = this.getprofessionalDetails(user.profileDetails.profileReq.professionalDetails)
+        const addressDetails = this.getPostalAdress(user.profileDetails.profileReq.personalDetails)
         activeUsersData.push({
           fullName: user ? `${user.firstName} ${user.lastName}` : null,
           email: user.personalDetails && user.personalDetails.primaryEmail ? user.personalDetails.primaryEmail : user.email,
           userId: user.id,
           active: !user.isDeleted,
           blocked: user.blocked,
-          designation: professionalDetails.designation
+          designation: professionalDetails.designation,
+          state: addressDetails.state,
+          city: addressDetails.city
         })
       })
     }
@@ -33,5 +36,16 @@ export class UtilityService {
       }, professionalDetails)
     }
     return professionalDetails
+  }
+  getPostalAdress(data: any) {
+    const addressDetails: any = {}
+    if (data && _.get(data, 'postalAddress')) {
+      const postalAddress = _.split(_.get(data, 'postalAddress'), ',')
+      addressDetails['country'] = postalAddress[0]
+      addressDetails['state'] = postalAddress[1]
+      addressDetails['city'] = postalAddress[2]
+
+    }
+    return addressDetails
   }
 }
